1f2ae8b54a5584747865f5548f52548e25a762a1
Anton Tanderup Adamsen <antontandrup@gmail.com>
2025-03-07 18:46:18 +0100

    Move to styled

--- src/components/MediocreMassiveMediaPlayerCard/MediocreMassiveMediaPlayerCard.tsx
+++ src/components/MediocreMassiveMediaPlayerCard/MediocreMassiveMediaPlayerCard.tsx
@@ -1,7 +1,8 @@
-import { createContext, Fragment } from "preact";
+import { createContext } from "preact";
 import { HomeAssistant } from "custom-card-helpers";
 import { AlbumArt, Title, Track } from "./components";
 import { PlaybackControls } from "./components/PlaybackControls";
+import styled from "styled-components";
 
 export type MediocreMassiveMediaPlayerCardConfig = {
   entity_id: string;
@@ -15,25 +16,20 @@ export const MediocreMassiveMediaPlayerCardContext = createContext<{
   config: null,
 });
 
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  justify-content: space-around;
+`;
+
 export const MediocreMassiveMediaPlayerCard = () => {
   return (
-    <Fragment>
-      <style>
-        {`
-        .root {
-          display: flex;
-          flex-direction: column;
-          gap: 16px;
-          justify-content: space-around;
-        }
-        `}
-      </style>
-      <div className="root">
-        <AlbumArt />
-        <Title />
-        <Track />
-        <PlaybackControls />
-      </div>
-    </Fragment>
+    <Root>
+      <AlbumArt />
+      <Title />
+      <Track />
+      <PlaybackControls />
+    </Root>
   );
 };
